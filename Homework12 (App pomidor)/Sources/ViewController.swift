a6271e9811606f3a8b2df2e3a25840d5a55cf1c7
Mikhail Laty <laty.mischa@gmail.com>
2023-06-21 20:25:26 +0300

    Add label and button, change background color.

--- Homework12 (App pomidor)/Sources/ViewController.swift
+++ Homework12 (App pomidor)/Sources/ViewController.swift
@@ -11,16 +11,48 @@ class ViewController: UIViewController {
 
     // MARK: - UI Elements
 
+    private lazy var timer: UILabel = {
+        let timer = UILabel()
+        timer.text = "25:00"
+        timer.font = .systemFont(ofSize: 84, weight: .bold)
+        timer.textAlignment = .center
+        timer.textColor = .black
+        timer.translatesAutoresizingMaskIntoConstraints = false
+        return timer
+    }()
+
+    private lazy var playOrPause: UIButton = {
+        let playOrPause = UIButton(type: .system)
+        let playIcon = UIImage(named: "playRed")
+        playOrPause.setImage(playIcon, for: .normal)
+        playOrPause.titleLabel?.font = .systemFont(ofSize: 20, weight: .heavy)
+        playOrPause.translatesAutoresizingMaskIntoConstraints = false
+        return playOrPause
+    }()
+
+
+
 
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupView()
+        setupHierarchy()
+        setupLayout()
+    }
+
+    // MARK: - Setups
+    private func setupView() {
+        view.backgroundColor = .black
+    }
+
+    private func setupHierarchy() {
 
     }
 
+    private func setupLayout() {
 
-    // MARK: - Setups
-
+    }
 
     // MARK: - Actions
 
