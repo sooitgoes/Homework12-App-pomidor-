25d878f132929af127d6d2708b28eb77f72f76fe
Mikhail Laty <laty.mischa@gmail.com>
2023-06-21 20:02:23 +0300

    Add icons and configure viewController.

--- Homework12 (App pomidor)/Sources/ViewController.swift
+++ Homework12 (App pomidor)/Sources/ViewController.swift
@@ -9,10 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - UI Elements
+
+
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
+
+
+    // MARK: - Setups
+
+
+    // MARK: - Actions
 
 
 }
