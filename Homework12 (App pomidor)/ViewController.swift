58cfaa5a629d087742a6b5f21dddeda1e14c6745
Mikhail Laty <laty.mischa@gmail.com>
2023-06-21 19:25:18 +0300

    Initial Commit

---
+++ Homework12 (App pomidor)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework12 (App pomidor)
+//
+//  Created by Михаил Латий on 21.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
